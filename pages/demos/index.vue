41e5ce890c76935fa93e5273901e73946046c8fd
zhangcong <zhangcong@9linktech.com>
2023-06-01 17:55:46 +0800

    style: 修改部分样式

--- pages/demos/index.vue
+++ pages/demos/index.vue
@@ -11,13 +11,13 @@ const { data, pending } = await useLazyAsyncData('demos', async () => await getD
 <template>
   <div h-full w-full>
     <ElScrollbar>
-      <div m-8 columns-4 gap-4>
+      <div m-8 columns-5 gap-4>
         <div
           v-for="demo, index in data" :key="demo._id" class="slide-enter mb-4 rounded transition duration-300 hover:scale-105" b="~ solid $bl-title" :style="{
             '--stage': index,
           }"
         >
-          <NuxtLink :to="demo.link" target="_blank" inline-block divide="y current">
+          <NuxtLink :to="demo.link" target="_blank" inline-block divide="y current" w-full>
             <div p-2>
               <img :src="demo.poster" min-h-10 w-full rounded>
             </div>
